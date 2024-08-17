50015d160f8765caa30623a3573ee87983307274
Ezeabasili, Anthony Etienne <anthony.ezeabasili@jenoptik.com>
2024-08-17 17:52:17 +0200

    removed comments

--- schema.sql
+++ schema.sql
@@ -1,22 +1,3 @@
--- DROP TABLE IF EXISTS todos;
-
--- CREATE TABLE todos (
---     id serial PRIMARY KEY,
---     user_id BIGINT NULL,
---     todo  TEXT NOT NULL,
--- );
-
--- DROP TABLE IF EXISTS users;
-
--- CREATE TABLE users (
---     id TEXT PRIMARY KEY,
---     firstname TEXT NOT NULL,
---     lastname TEXT NOT NULL,
---     -- password TEXT NOT NULL,
---     emai TEXT UNIQUE NOT NULL,
--- );
-
-
 CREATE TABLE IF NOT EXISTS todos (
   id serial PRIMARY KEY,
   user_id BIGINT NULL,
